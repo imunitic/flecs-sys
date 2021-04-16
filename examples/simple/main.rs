bd851da31c9a46f63e4fc1b69b9396433db4ec4d
Ivica Munitic <imunitic@gmail.com>
2021-04-16 20:37:48 +0200

    fix: Move examples to its proper folder
    - Add velocity component to the example
    
    Closes #3

---
+++ examples/simple/main.rs
@@ -0,0 +1,80 @@
+use flecs_sys::*;
+use std::ffi::CString;
+use std::mem::*;
+
+#[repr(C)]
+struct Position {
+    x: f32,
+    y: f32,
+}
+#[repr(C)]
+struct Velocity {
+    x: f32,
+    y: f32,
+}
+
+unsafe extern "C" fn move_sys(it: *mut ecs_iter_t) {
+    let count = (*it).count as usize;
+    let posc = ecs_column_w_size(it, size_of::<Position>(), 1);
+    let velc = ecs_column_w_size(it, size_of::<Velocity>(), 2);
+    let p = std::ptr::slice_from_raw_parts_mut(posc as *mut Position, count)
+        .as_mut()
+        .unwrap();
+    let v = std::ptr::slice_from_raw_parts_mut(velc as *mut Velocity, count)
+        .as_mut()
+        .unwrap();
+    for i in 0..count {
+        let position = p.get_mut(i).unwrap();
+        let velocity = v.get_mut(i).unwrap();
+        position.x += velocity.x * (*it).delta_time;
+        position.y += velocity.y * (*it).delta_time;
+
+        println!("Moved to {} {}", position.x, position.y);
+    }
+}
+
+fn main() {
+    unsafe {
+        let world = ecs_init();
+        let pos_name = CString::new("Position").unwrap();
+        let pos_id = ecs_new_component(
+            world,
+            0,
+            pos_name.as_ptr(),
+            size_of::<Position>(),
+            align_of::<Position>(),
+        );
+
+        let vel_name = CString::new("Velocity").unwrap();
+        let vel_id = ecs_new_component(world, 
+            0, 
+            vel_name.as_ptr(), 
+            size_of::<Velocity>(), 
+            align_of::<Velocity>(),
+        );
+
+        let comps = CString::new("Position, Velocity").unwrap();
+        let sys_name = CString::new("Move").unwrap();
+        ecs_new_system(
+            world,
+            0,
+            sys_name.as_ptr(),
+            EcsOnUpdate as u64,
+            comps.as_ptr(),
+            Some(move_sys),
+        );
+
+        let ename = CString::new("MyEntity").unwrap();
+        let entity = ecs_new_entity(world, 0, ename.as_ptr(), comps.as_ptr());
+        
+        let mut p = Position { x: 0.0, y: 0.0 };
+        let p_ptr: *mut std::ffi::c_void = &mut p as *mut _ as *mut std::ffi::c_void;
+        ecs_set_ptr_w_id(world, entity, pos_id, size_of::<Position>(), p_ptr);
+
+        let mut v = Velocity { x: 1.0, y: 1.0 };
+        let v_ptr: *mut std::ffi::c_void = &mut v as *mut _ as *mut std::ffi::c_void;
+        ecs_set_ptr_w_id(world, entity, vel_id, size_of::<Velocity>(), v_ptr);
+
+        while ecs_progress(world, 0.0) {}
+    }
+}
